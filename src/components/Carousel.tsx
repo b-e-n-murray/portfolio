bc5e8d405f119ec3b836d5bd2d7f83fe368afb98
Ben Murray <benmurray573@gmail.com>
2023-01-23 15:51:38 +0000

    somewhat soved carousel sizing

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -12,7 +12,7 @@ function Carousel(): JSX.Element {
   return (
     <div className="all-carousel">
       <CarouselProvider
-        naturalSlideWidth={50}
+        naturalSlideWidth={150}
         naturalSlideHeight={20}
         totalSlides={4}
         currentSlide={0}
@@ -20,69 +20,75 @@ function Carousel(): JSX.Element {
         interval={5000}
         className="carousel"
       >
-        <Slider>
-          <Slide index={0} className="slide">
-            <div className="proj-item">
-              <img
-                className="app-icons"
-                src="https://static.thenounproject.com/png/1925938-200.png"
-                alt="gymbuddy"
-              ></img>
-              <a
-                className="proj-titles"
-                href="https://b-e-n-murray-gym-buddy.netlify.app/"
-              >
-                Gym-Buddy
-              </a>
-            </div>
-          </Slide>
-          <Slide index={1} className="slide">
-            <div className="proj-item">
-              <img
-                className="app-icons"
-                src="https://icons.veryicon.com/png/o/animal/pet-icon/dog-24.png"
-                alt="dog"
-              ></img>
-              <a
-                className="proj-titles"
-                href="https://dog-breed-voter-mtkb.netlify.app/"
-              >
-                Dog Breed Voting
-              </a>
-            </div>
-          </Slide>
-          <Slide index={2} className="slide">
-            <div className="proj-item"></div>
-            <img
-              className="app-icons"
-              src="https://cdn-icons-png.flaticon.com/512/2769/2769646.png"
-              alt="scissors"
-            ></img>
-            <a className="proj-titles" href="https://snip-snap-pro.netlify.app/">
-              SnipSnap
-            </a>
-          </Slide>
-          <Slide index={3} className="slide">
-            <div className="proj-item"></div>
-            <img
-              className="app-icons"
-              src="https://cdn-icons-png.flaticon.com/512/282/282163.png"
-              alt="notepad"
-            ></img>
-            <a
-              className="proj-titles"
-              href="https://b-e-n-murray-todo-react.netlify.app/"
-            >
-              To-Do App
-            </a>
-          </Slide>
-        </Slider>
-        <ButtonBack className="car-btn">Back</ButtonBack>
-        <Dot slide={0}></Dot>
-        <Dot slide={1}></Dot>
-        <Dot slide={2}></Dot>
-        <Dot slide={3}></Dot>
-        <ButtonNext className="car-btn">Next</ButtonNext>
+        <div className="slides-btns">
+          <Slider>
+            <Slide index={0} className="slide">
+              <div className="proj-item">
+                <img
+                  className="app-icons"
+                  src="https://static.thenounproject.com/png/1925938-200.png"
+                  alt="gymbuddy"
+                ></img>
+                <a
+                  className="proj-titles"
+                  href="https://b-e-n-murray-gym-buddy.netlify.app/"
+                >
+                  Gym-Buddy
+                </a>
+              </div>
+            </Slide>
+            <Slide index={1} className="slide">
+              <div className="proj-item">
+                <img
+                  className="app-icons"
+                  src="https://icons.veryicon.com/png/o/animal/pet-icon/dog-24.png"
+                  alt="dog"
+                ></img>
+                <a
+                  className="proj-titles"
+                  href="https://dog-breed-voter-mtkb.netlify.app/"
+                >
+                  Dog Breed Voting
+                </a>
+              </div>
+            </Slide>
+            <Slide index={2} className="slide">
+              <div className="proj-item">
+                <img
+                  className="app-icons"
+                  src="https://cdn-icons-png.flaticon.com/512/2769/2769646.png"
+                  alt="scissors"
+                ></img>
+                <a className="proj-titles" href="https://snip-snap-pro.netlify.app/">
+                  SnipSnap
+                </a>
+              </div>
+            </Slide>
+            <Slide index={3} className="slide">
+              <div className="proj-item">
+                <img
+                  className="app-icons"
+                  src="https://cdn-icons-png.flaticon.com/512/282/282163.png"
+                  alt="notepad"
+                ></img>
+                <a
+                  className="proj-titles"
+                  href="https://b-e-n-murray-todo-react.netlify.app/"
+                >
+                  To-Do App
+                </a>
+              </div>
+            </Slide>
+          </Slider>
+          <div className="nav-btns">
+            <ButtonBack className="car-btn">Back</ButtonBack>
+            <Dot slide={0}></Dot>
+            <Dot slide={1}></Dot>
+            <Dot slide={2}></Dot>
+            <Dot slide={3}></Dot>
+            <ButtonNext className="car-btn">Next</ButtonNext>
+          </div>
+        </div>
       </CarouselProvider>
     </div>
   );
